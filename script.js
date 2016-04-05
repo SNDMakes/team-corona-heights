1de5889ffdf889a013bd53d4fc80a3d9f3efe6e7
Ashley Wu <ashleywu2016@u.northwestern.edu>
2016-04-05 15:55:29 -0700

    various adjustments

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ $( document ).ready(function() {
 
     $('#submit').on('click',function(){
     	var input = $('#search-box').val();
-    	// var input_parse = input.split(" ");
-
     	var base_url = 'http://clips.gifs.is/?q=';
 
     	location.href = base_url + input;
-
+    	console.log(input);
     });
+
+ //    $("#search-box").onkeydown = function(event){
+ //    	if(event.keyCode == 13){
+ //        	var input = $('#search-box').val();
+	//     	var base_url = 'http://clips.gifs.is/?q=';
+
+	//     	location.href = base_url + input;
+	//     	console.log(input);
+ //    	}
+	// };
 
 });
